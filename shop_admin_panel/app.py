354fcdff48fd509f66036b15decc5f9801b30976
unknown <hosein467600@gmail.com>
2025-08-20 13:02:25 +0330

    Initial commit

--- shop_admin_panel/app.py
+++ shop_admin_panel/app.py
@@ -525,7 +525,7 @@ with app.app_context():
 
 
 # ---------- اجرا ----------
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     with app.app_context():
+#         db.create_all()
+#     app.run(debug=True)

443bd2888626bf71a2c4495868361220bfb9870e
unknown <hosein467600@gmail.com>
2025-09-23 13:57:58 +0330

    ove run.py to root and fix imports for ender

---
+++ project-root/run.py
@@ -0,0 +1,8 @@
+from clinic_reservation.app import create_app, db
+from flask_migrate import Migrate, upgrade
+
+app = create_app()
+migrate = Migrate(app, db)
+
+with app.app_context():
+    upgrade()

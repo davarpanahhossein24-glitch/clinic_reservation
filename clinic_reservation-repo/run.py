65b5f95d8998fbe7aad8ca1d90a647df8bfe9fb0
unknown <hosein467600@gmail.com>
2025-09-29 13:17:05 +0330

    Fix: move create_app to clinic_reservation/__init__.py

--- clinic_reservation-repo/run.py
+++ clinic_reservation-repo/run.py
@@ -1,8 +1,7 @@
-from clinic_reservation.app import create_app
+from clinic_reservation import create_app
 
-# ایجاد اپلیکیشن
 app = create_app()
 
 if __name__ == "__main__":
-    # حالت debug برای توسعه محلی
     app.run(host="0.0.0.0", port=8000)
+
